c50d6d2efaae3226a541070acd34e2c1ab64c10f
Roman <romazaniosdev@gmail.com>
2026-04-13 10:00:38 +0300

    add view

---
+++ 133DradigoxBorsexslan/Router/SaveService.swift
@@ -0,0 +1,15 @@
+//
+//  SaveService.swift
+//  133DradigoxBorsexslan
+//
+
+
+import Foundation
+
+struct SaveService {
+    
+    static var lastUrl: URL? {
+        get { UserDefaults.standard.url(forKey: "LastUrl") }
+        set { UserDefaults.standard.set(newValue, forKey: "LastUrl") }
+    }
+}
